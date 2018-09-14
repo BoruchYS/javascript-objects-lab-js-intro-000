035016465e8901e7ab5e96e1e717cf8b0cda6328
barry.c.schechter@gmail.com <barry.c.schechter@gmail.com>
2018-09-14 15:19:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var recipes = {sugar: '1 cup',
                butter: '1 stick',
                water: '1 cup'};
 function updateObjectWithKeyandValue(object,key,value){
-  return object.assign({[key]:value}, object)
+  return object.key = value
 }
