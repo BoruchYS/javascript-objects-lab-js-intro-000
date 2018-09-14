4c46935313ce09827817bf3912140a97012fdd60
barry.c.schechter@gmail.com <barry.c.schechter@gmail.com>
2018-09-14 15:18:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,6 @@
+var recipes = {sugar: '1 cup',
+               butter: '1 stick',
+               water: '1 cup'};
+function updateObjectWithKeyandValue(object,key,value){
+  return object.assign({[key]:value}, object)
+}
